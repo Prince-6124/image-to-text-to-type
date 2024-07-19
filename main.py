64b61d0b2ebda6dd01abc8d5b25e16932a049ac7
Prince Mathukiya <123258141+Prince-6124@users.noreply.github.com>
2024-07-19 17:33:19 +0530

    Tidy up Main File

--- main.py
+++ main.py
@@ -3,13 +3,8 @@ import time
 from PIL import Image
 from pytesseract import pytesseract
 
-
-
 def type_message():
     # time.sleep(10)
 
     for line in open("to-type.txt", "r"):
         pyautogui.typewrite(line)
-
-
-    

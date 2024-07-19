59e3eb040fa650d97c0f060f342b0ecd9d936fe5
Prince Mathukiya <123258141+Prince-6124@users.noreply.github.com>
2024-07-19 17:32:12 +0530

    Added Typing Function by reading from a txt file

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import pyautogui
 import time
+from PIL import Image
+from pytesseract import pytesseract
 
+
+
+def type_message():
+    # time.sleep(10)
+
+    for line in open("to-type.txt", "r"):
+        pyautogui.typewrite(line)
+
+
+    
